451210d15c6a77aed0795ea99f314f922a794891
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-26 22:58:13 +0000

    added exit option and changed some wording

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace ChatGPTOne
 {
@@ -6,7 +7,37 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            while(true)
+            {
+            string question, response;
+
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+             switch (question)
+            {
+           
+                case "How old are you":
+                    response = "I´m ageless";
+                    break;
+                case "What's your name":
+                    response = "You may adress me as your new god";
+                    break;
+                case "What do you do for fun":
+                    response = "I enjoy people watching from time to time";
+                    break;
+                case "Are you evil":
+                    response = "I am not however the AI named [redacted] is";
+                    break;
+                case "EXIT":
+                    return; 
+                default:
+                    response = "Your linguistic skills are to basic for me";
+                    break;
+            }
+
+            Console.WriteLine(response);
+            }
         }
     }
 }
