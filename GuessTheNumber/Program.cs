8c8a12d0f162e46278c9bb5204da4d93e68abbca
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-28 17:46:32 +0000

    addedGuessTheNumber with the final code

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+          
+
+             string question, response;
+
+             
+
+             Random rnd = new Random();
+             int numberToGuess = rnd.Next(31);
+
+             int turns = 0;
+             while(true)
+             {
+
+             Console.Write("Insert number ");
+             question = Console.ReadLine();
+
+             int s =int.Parse(question);
+
+            
+
+
+
+
+                if (s < 0 | s > 30)
+                {
+                 Console.WriteLine("Numbers must be between 0 and 30. Try again.");
+                 turns++;
+                }
+                else if (s<numberToGuess)
+                {
+                    Console.WriteLine($"The hidden number is higher than{s}. Try Again.");
+                    turns++;
+                }
+                else if(s>numberToGuess)
+                {
+                 Console.WriteLine($"The number is lower than {s}. Try Again.");
+                 turns++;
+                }
+
+                else if (s == numberToGuess)
+                {
+                Console.WriteLine($"You found the hidden number{numberToGuess} after {turns} tries.");
+                turns++;
+                break;
+                }
+
+             }
+            
+            
+                
+
+                
+            
+
+
+        } 
+    }
+}
